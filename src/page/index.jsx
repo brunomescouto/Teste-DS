01ff39ec9e53c2b844925c21cbb8b375e64f525a
brunomescouto <bruno.mescouto@gmail.com>
2024-09-25 13:17:43 -0300

    Add files via upload

---
+++ src/page/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <button className='frame'>
+        <div className='iconos-varios-lapiz'>
+          <div className='vector' />
+        </div>
+        <div className='texto'>
+          <span className='botones'>Botones</span>
+        </div>
+      </button>
+    </div>
+  );
+}
